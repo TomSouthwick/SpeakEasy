28bc95dd99968192f9e9b0a657aece795faf06c2
Tom Southwick <thomas.h.southwick@gmail.com>
2022-01-26 00:27:43 +1100

    connec to mlab

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/tech-friends',
+  `mongodb+srv://${process.env.MLAB_USER}:${process.env.MLAB_PW}@cluster0.j56u5.mongodb.net/Cluster0?retryWrites=true&w=majority`,
+  // process.env.MONGODB_URI || "mongodb://localhost/tech-friends",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
